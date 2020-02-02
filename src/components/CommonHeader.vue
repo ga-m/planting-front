8df867836b9ec67adf97d77253aa87492c00ab00
jinyeong <jinyp91@gmail.com>
2020-02-02 10:40:15 +0900

    add element

--- src/components/CommonHeader.vue
+++ src/components/CommonHeader.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="header">
+    <i class="el-icon-heavy-rain main-icon"></i>
+    <span class="title">Planting</span>
+    <div class="search-bar">
+      <el-input
+        placeholder="Please input"
+        class="input"
+      >
+        <el-button slot="append" icon="el-icon-search"></el-button>
+      </el-input>
+    </div>
     <router-link to="/home">
       <i class="el-icon-s-home custom-icon"></i>
     </router-link>
@@ -13,9 +23,7 @@
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style scoped>
@@ -24,6 +32,17 @@ export default {
   height: 75px;
   box-shadow: 0 0 10px 0 rgba(148, 148, 148, 0.5);
   background-color: #ffffff;
+  display: flex;
+}
+.main-icon {
+  color: #979797;
+  font-size: 30px;
+}
+.title {
+  width: 79px;
+  height: 18px;
+  font-size: 18px;
+  color: #7f7f7f;
 }
 .custom-icon {
   color: #cfcfcf;
@@ -31,5 +50,8 @@ export default {
 }
 .router-link-exact-active > .custom-icon {
   color: #5a9c50;
+}
+.el-input {
+  width: 300px;
 }
 </style>
